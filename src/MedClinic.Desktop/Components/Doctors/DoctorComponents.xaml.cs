f7e50b3e2f0ea9775918fa5dd6e3a110cbbc3390
ShodiyorErqoziyev <shodiyor918@gmail.com>
2025-03-07 15:25:29 +0500

    Update

---
+++ src/MedClinic.Desktop/Components/Doctors/DoctorComponents.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MedClinic.Desktop.Components.Doctors
+{
+    /// <summary>
+    /// Interaction logic for DoctorComponents.xaml
+    /// </summary>
+    public partial class DoctorComponents : UserControl
+    {
+        public DoctorComponents()
+        {
+            InitializeComponent();
+        }
+    }
+}
