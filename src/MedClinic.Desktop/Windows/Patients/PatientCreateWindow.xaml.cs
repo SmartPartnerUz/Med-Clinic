a9ffcfd113c0e6cdde4ebde2c6779910b79d4727
ShodiyorErqoziyev <shodiyor918@gmail.com>
2025-03-17 16:09:38 +0500

    PatientCreateWindow

--- src/MedClinic.Desktop/Windows/Patients/PatientCreateWindow.xaml.cs
+++ src/MedClinic.Desktop/Windows/Patients/PatientCreateWindow.xaml.cs
@@ -1,27 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using static MedClinic.Desktop.Windows.BlurWindow.BlurEffect;
+using System.Windows.Interop;
+using System.Text.RegularExpressions;
 
-namespace MedClinic.Desktop.Windows.Patients
+namespace MedClinic.Desktop.Windows.Patients;
+
+/// <summary>
+/// Interaction logic for PatientCreateWindow.xaml
+/// </summary>
+public partial class PatientCreateWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for PatientCreateWindow.xaml
-    /// </summary>
-    public partial class PatientCreateWindow : Window
+    public PatientCreateWindow()
     {
-        public PatientCreateWindow()
+        InitializeComponent();
+    }
+
+    [DllImport("user32.dll")]
+    internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+    internal void EnableBlur()
+    {
+        var windowHelper = new WindowInteropHelper(this);
+
+        var accent = new AccentPolicy
         {
-            InitializeComponent();
-        }
+            AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND,
+            AccentFlags = 2,
+            GradientColor = unchecked((int)0x80000000),
+            AnimationId = 0
+        };
+        accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+
+        var accentStructSize = Marshal.SizeOf(accent);
+
+        var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+        Marshal.StructureToPtr(accent, accentPtr, false);
+
+        var data = new WindowCompositionAttributeData();
+        data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+        data.SizeOfData = accentStructSize;
+        data.Data = accentPtr;
+
+        SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+        Marshal.FreeHGlobal(accentPtr);
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        EnableBlur();
+    }
+
+    private void Close_Button_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
+    private void txt_PhoneNuber_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+    {
+        e.Handled = !Regex.IsMatch(e.Text, "^[0-9]+$");
+    }
+
+    private void DatePicker_SelectedDateChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+
+    }
+
+    private void Save_Button_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
